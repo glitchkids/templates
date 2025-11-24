1127f362fbebe3480981a8d4e2f6bd96e81f6806
shaduin <shaduin@glitchkids.space>
2025-11-24 18:32:46 +0100

    update unocss config

--- templates/sveltekit-unocss/unocss.config.ts
+++ templates/sveltekit-unocss/unocss.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'unocss';
+import {
+	defineConfig,
+	presetTypography,
+	transformerDirectives,
+	presetWind4,
+	presetIcons
+} from 'unocss';
 
-import presetIcons from '@unocss/preset-icons';
-import presetWind4 from '@unocss/preset-wind4';
 import { presetFluidSizing } from 'unocss-preset-fluid-sizing';
 import { presetGrid } from 'unocss-preset-grid';
 
 export default defineConfig({
-	presets: [presetWind4(), presetIcons(), presetFluidSizing(), presetGrid()]
+	presets: [presetTypography(), presetWind4(), presetIcons(), presetFluidSizing(), presetGrid()],
+	transformers: [transformerDirectives()]
 });
